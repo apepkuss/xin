4606fa63c8b7dec5cfcfe43a69bca71867e4b592
Xin Liu <sam@secondstate.io>
2023-09-13 20:12:40 +0800

    feat(common): implement `Usage` struct
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/common.rs
+++ src/common.rs
@@ -5,3 +5,13 @@ pub enum LlamaCppLogitBiasType {
     InputIds,
     Tokens,
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Usage {
+    /// Number of tokens in the prompt.
+    prompt_tokens: u32,
+    /// Number of tokens in the generated completion.
+    completion_tokens: u32,
+    /// Total number of tokens used in the request (prompt + completion).
+    total_tokens: u32,
+}
